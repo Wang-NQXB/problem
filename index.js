aa3806a2e10199c2b52eaa71250d0afe25f24238
王跃 <hust_wangyue@foxmail.com>
2017-11-27 13:06:41 +0800

    description

--- index.js
+++ index.js
@@ -176,6 +176,17 @@ class LRUCache {
   }
 }
 
+/**
+ * Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+ *
+ * For example,
+ * Given [100, 4, 200, 1, 3, 2],
+ * The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
+ *
+ * https://leetcode.com/problems/longest-consecutive-sequence/description/
+ * @param nums
+ * @returns {*}
+ */
 const longestConsecutive = nums => {
   nums.sort((a, b) => a - b)
 
